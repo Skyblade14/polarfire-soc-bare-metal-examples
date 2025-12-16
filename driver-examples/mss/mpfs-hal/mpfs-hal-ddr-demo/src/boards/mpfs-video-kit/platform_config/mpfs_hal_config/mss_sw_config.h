9976cd0e4c851be2b3340270057a27b3a361ed9b
Skyblade14 <62959942+Skyblade14@users.noreply.github.com>
2025-12-16 10:03:05 +0100

    Update mss_sw_config.h

--- driver-examples/mss/mpfs-hal/mpfs-hal-ddr-demo/src/boards/mpfs-video-kit/platform_config/mpfs_hal_config/mss_sw_config.h
+++ driver-examples/mss/mpfs-hal/mpfs-hal-ddr-demo/src/boards/mpfs-video-kit/platform_config/mpfs_hal_config/mss_sw_config.h
@@ -200,8 +200,8 @@
  * If you need to edit this function, make a copy of the function without the
  * weak declaration in your application code.
  * */
-#define DEBUG_DDR_INIT
-#define DEBUG_DDR_RD_RW_FAIL
+//#define DEBUG_DDR_INIT
+//#define DEBUG_DDR_RD_RW_FAIL
 //#define DEBUG_DDR_RD_RW_PASS
 //#define DEBUG_DDR_CFG_DDR_SGMII_PHY
 //#define DEBUG_DDR_DDRCFG
